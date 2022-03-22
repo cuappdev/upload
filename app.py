a0a64c8a624da587d3d749b3a1747e339deb4f04
Kate Liang <ksl67@cornell.edu>
2022-03-21 20:57:05 -0400

    Allow users to remove images (#4)

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import json
 
 from flask import Flask
 from flask import request
-from utils import success_response, failure_response, upload_image_helper
+from utils import success_response, failure_response, upload_image_helper, remove_image_helper
 import os
 
 app = Flask(__name__)
@@ -17,7 +17,7 @@ def hello_world():
 def upload():
     body = json.loads(request.data)
     image_data = body.get("image")
-    bucket_name = body.get("bucket")  # we only support pear right now
+    bucket_name = body.get("bucket")  
     if image_data is None:
         return failure_response("No base64 URL to be found!")
     img_url = upload_image_helper(image_data, bucket_name)
@@ -25,7 +25,20 @@ def upload():
         return failure_response("Could not upload image!")
     return success_response(img_url, 201)
 
+@app.route("/remove/", methods=["POST"])
+def remove():
+    body = json.loads(request.data)
+    image_url = body.get("image_url")
+    bucket_name = body.get("bucket") 
+    if image_url is None or image_url == "":
+        return failure_response("No URL to be found!")
+    res = remove_image_helper(image_url, bucket_name)
+    if not res:
+        return failure_response("Image does not exist!")
+    elif res != 204:
+        return failure_response("Could not delete image!")
+    return success_response("Image successfully deleted!")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
